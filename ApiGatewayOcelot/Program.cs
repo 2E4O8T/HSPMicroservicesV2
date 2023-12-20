a47c9eed1a7eb56b055f5fbd2e8ed077188420f5
2E4O8T <benoit.froidevan@axa.fr>
2023-12-20 13:42:02 +0100

    Configure-Api-Gateway-Ocelot

--- ApiGatewayOcelot/Program.cs
+++ ApiGatewayOcelot/Program.cs
@@ -1,3 +1,6 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add ocelot configuration file and injecting ocelot service
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
@@ -21,5 +28,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Ocelot
+await app.UseOcelot();
 
 app.Run();
